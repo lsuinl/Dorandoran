fc854a839fbe0dbfc7820ed155608f23ab7bea9c
lsuinl <su96in43@naver.com>
2023-01-07 22:06:29 +0900

    init project

---
+++ lib/screen/sign_up.dart
@@ -0,0 +1,12 @@
+//사진권한.
+//위치권한.
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
