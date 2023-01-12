a374d1a7d01758e76f16e4deb49d2bc580ef7112
lsuinl <su96in43@naver.com>
2023-01-13 05:33:11 +0900

    firebase base

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:dorandoran/screen/kakao_login.dart';
 import 'package:dorandoran/screen/using_agree.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 
-Future<void> main() async {
+void main() async{
   KakaoSdk.init(nativeAppKey: 'd54cd76337470f87b093bfdadfa53292');
+ WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -19,3 +19,12 @@ Future<void> main() async {
   );
 
 }
+
+Future<void> initializefirebase() async {
+  //FirebaseApp app = await Firebase.initializeApp();
+  await Firebase.initializeApp();
+  //print('Initialized default app $app');
+}
+// await Firebase.initializeApp(
+// options: DefaultFirebaseOptions.currentPlatform,
+// );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import firebase_auth
 import firebase_core
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
