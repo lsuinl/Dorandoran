44fc45db927bc2f38a40a5dd722ddde6ea65cae8
lsuinl <su96in43@naver.com>
2023-02-01 21:32:16 +0900

    카드에 데이터 받아서 띄우기(+시간 계산) ㅑ

---
+++ lib/model/postcard.dart
@@ -0,0 +1,35 @@
+class postcard {
+  final int postId;
+  final String contents;
+  final String postTime;
+  final int location;
+  final int likeCnt;
+  final bool likeResult;
+  final String backgroundPicUri;
+  final int? replyCnt;
+
+
+  postcard(
+      {required this.postId,
+        required this.contents,
+        required this.postTime,
+        required this.location,
+        required this.likeCnt,
+        required this.likeResult,
+        required this.backgroundPicUri,
+        required this.replyCnt,
+       });
+
+  factory postcard.fromJson(Map<String, dynamic> json) {
+    return postcard(
+      postId: json["postId"],
+      contents: json["contents"],
+      postTime: json["postTime"],
+      location: json["location"],
+      likeCnt: json["likeCnt"],
+      likeResult: json["likeResult"],
+      backgroundPicUri: json["backgroundPicUri"],
+      replyCnt: json["replyCnt"],
+    );
+  }
+}
