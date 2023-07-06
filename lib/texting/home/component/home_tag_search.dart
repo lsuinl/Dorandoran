7b6fb0a38543fb6db0113274bf7a8ba678719b09
lsuinl <su96in43@naver.com>
2023-07-06 20:12:08 +0900

    feat: 해시태그 검색 만들기(하단 오버레이 추가 필요함)

--- lib/texting/home/component/home_tag_search.dart
+++ lib/texting/home/component/home_tag_search.dart
@@ -1,4 +1,6 @@
+import 'package:dorandoran/texting/home/model/search_hash.dart';
 import 'package:flutter/material.dart';
+import '../quest/get_search_hash.dart';
 
 // 검색을 위해 앱의 상태를 변경해야하므로 StatefulWidget 상속
 class TagSearch extends StatefulWidget {
@@ -9,22 +11,53 @@ class TagSearch extends StatefulWidget {
 }
 
 class TagSearchState extends State<TagSearch> {
-
+  TextEditingController searchTextController = TextEditingController();
+  List<Widget> searchresult=[];
+  bool showlist = false;
   @override
   Widget build(BuildContext context) {
     return Column(
           children: [
             TextFormField(
+              controller: searchTextController,
                 decoration: InputDecoration(
                     border: UnderlineInputBorder(),
                     icon: (Icon(Icons.search)),
-                    suffixIcon: Icon(Icons.cancel),
+                    suffixIcon: IconButton(
+                      onPressed: (){searchTextController.clear();},
+                        icon:Icon(Icons.cancel)),
                     hintText: "관심 태그를 추가해보세요",),
               onTap: (){
+                //검색결과나오는 창 만들기
+                print("눌림");
+                setState(() {
+                  showlist=true;
+                });
+              },
+              onEditingComplete: (){
+                setState(() {
+                  showlist=false;
+                });
+              },
+              onChanged: (text) async {
+                //검색결과창의 데이터 계속해서 변경하기
+                List<searchHash> list = await GetSearchHash(text);
+                print(list.map((e) => e.hashTagName));
+                setState(() {
+                  searchresult = list.map((e) => Column(
+                    children: [
+                      Text(e.hashTagName),
+                      Text("게시글 수: ${e.hashTagCount}"),
+                      e.hashTagCheck==true ? IconButton(onPressed: (){}, icon: Icon(Icons.add)): IconButton(onPressed: (){}, icon: Icon(Icons.cancel))
+                    ],)
+                  ).toList();
+                });
+
               },
             ),
           ],
         );
   }
-}
+
+  }
 
---
+++ lib/texting/home/quest/get_search_hash.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:dorandoran/texting/home/model/popular_hash.dart';
+import 'package:dorandoran/texting/home/model/search_hash.dart';
+import 'package:http/http.dart' as http;
+import 'package:dorandoran/common/uri.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+//해시태그 검색하기
+Future<List<searchHash>> GetSearchHash(String text) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String accessToken = prefs.getString("accessToken")!;
+  http.Response response= await http.get(
+    Uri.parse('$urls/api/hashTag?hashTag=$text'),
+    headers: <String, String>{
+      'Content-Type': 'application/json',
+      'authorization':'Bearer $accessToken',
+    },
+  );
+    List<dynamic> body = jsonDecode(utf8.decode(response.bodyBytes));
+    List<searchHash> card = body.map((dynamic e) => searchHash.fromJson(e))
+        .toList();
+    return card;
+
+
+}
