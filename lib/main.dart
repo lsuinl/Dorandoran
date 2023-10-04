c4b4e9cc3c41438f8980fef452ed36f1a30e2ece
lsuinl <su96in43@naver.com>
2023-10-04 23:36:07 +0900

    feat: 서버 크리티컬 공지 기능

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ void main() async {
               child: child!
           );
         },
-        home:Login_check(),
+        home: Login_check(),
         //번영(영어.한국어)
         localizationsDelegates: [
           GlobalMaterialLocalizations.delegate,
